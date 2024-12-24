ba14b7d0a5ce82f451122fbfb0e1eefc4dbaa259
Gurubrahmanandam E <guru.prince8@gmail.com>
2024-12-24 12:25:31 -0600

    added table component

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import SimpleTable from "./components/SimpleTable";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+    <SafeAreaProvider>
+        <SafeAreaView style={styles.container} edges={['top']}>
+            <ScrollView style={styles.scrollView}>
+                {/*<Text style={styles.text}>*/}
+                {/*    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do*/}
+                {/*    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad*/}
+                {/*    minim veniam, quis nostrud exercitation ullamco laboris nisi ut*/}
+                {/*    aliquip ex ea commodo consequat. Duis aute irure dolor in*/}
+                {/*    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla*/}
+                {/*    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in*/}
+                {/*    culpa qui officia deserunt mollit anim id est laborum.*/}
+                {/*</Text>*/}
+                <SimpleTable></SimpleTable>
+            </ScrollView>
+        </SafeAreaView>
+    </SafeAreaProvider>
+);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+    },
+    scrollView: {
+        backgroundColor: 'white',
+    },
+    text: {
+        fontSize: 42,
+        padding: 12,
+    },
 });
+
+export default App;
